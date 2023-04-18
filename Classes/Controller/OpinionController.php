a14650c47d4f0f1cbb52c5c5b4da87a9917e182a
Volker Kemeter <v.kemeter@supseven.at>
2023-04-18 15:22:37 +0200

    [TASK] add flash messages

--- Classes/Controller/OpinionController.php
+++ Classes/Controller/OpinionController.php
@@ -15,6 +15,8 @@ use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Http\HtmlResponse;
 use TYPO3\CMS\Core\Mail\Mailer;
+use TYPO3\CMS\Core\Messaging\FlashMessage;
+use TYPO3\CMS\Core\Messaging\FlashMessageService;
 use TYPO3\CMS\Core\SingletonInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
@@ -75,12 +77,30 @@ class OpinionController extends ActionController
             $email = GeneralUtility::makeInstance(Email::class)->create($opinion);
 
             GeneralUtility::makeInstance(Mailer::class)->send($email);
+
+            $message = GeneralUtility::makeInstance(FlashMessage::class,
+                'The Message was sent successfully',
+                '[OPINION] Message sent.',
+                FlashMessage::OK,
+                true
+            );
         } catch (Exception|TransportExceptionInterface $e) {
+            $message = GeneralUtility::makeInstance(FlashMessage::class,
+                'The Message was not sent',
+                '[OPINION] Message not sent.',
+                FlashMessage::ERROR,
+                true
+            );
+
             $this->logger->error('FE-Mail send failed: {message} with code {code}', [
                 'message' => $e->getMessage(),
                 'code'    => $e->getCode(),
             ]);
         }
+
+        $flashMessageService = GeneralUtility::makeInstance(FlashMessageService::class);
+        $messageQueue = $flashMessageService->getMessageQueueByIdentifier();
+        $messageQueue->addMessage($message);
 
         return new HtmlResponse('');
     }
